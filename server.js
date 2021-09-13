aba352f33f4cbb50a61611c81853e6cfc2487446
julianmendoza <julianjohnmendoza@gmail.com>
2021-09-13 12:13:22 -0400

    trying to fix build

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 const path=require("path")
 app.use(express.json());
 var pgp = require("pg-promise")(/* options */);
-app.use(express.static(path.join(__dirname,"build")))
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"build")))
 }
