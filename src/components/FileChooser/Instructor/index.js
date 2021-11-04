d4afe09fb10d91d101a10c2bdd8f7fc9e1093a17
julianmendoza <julianjohnmendoza@gmail.com>
2021-11-03 22:11:32 -0400

    fix csv file on new line for last entry

--- src/components/FileChooser/Instructor/index.js
+++ src/components/FileChooser/Instructor/index.js
@@ -1,5 +1,4 @@
 import * as XLSX from "xlsx";
-import * as csvToJson from "convert-csv-to-json";
 import React from "react";
 export default class InstructorFileChooser extends React.Component {
   constructor(props) {
@@ -14,30 +13,32 @@ export default class InstructorFileChooser extends React.Component {
     var fileData = reader.result;
     var wb = XLSX.read(fileData, { type: "binary" });
     var data = {};
-    for(let i=0;i<wb.SheetNames.length;i++){
+    for (let i = 0; i < wb.SheetNames.length; i++) {
       var rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets["Sheet1"]);
       var rowString = JSON.stringify(rowObj);
       data[i] = rowString;
     }
     this.setState({ excelData: data });
   }
-  csvToJson(reader){
+  csvToJson(reader) {
     var fileData = reader.result;
-    var lines=fileData.split("\n")
-    var result = [];
-    var headers=lines[0].split(",");
-    var data = {};
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        var currentline=lines[i].split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-        result.push(obj);
+    var lines = fileData.split("\n");
+    if (lines[lines.length - 1] == "") {
+      lines.pop();
     }
-    data[0]=JSON.stringify(result)
+    var result = [];
+    var headers = lines[0].split(",");
+    var data = {};
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(",");
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+      result.push(obj);
+    }
+    data[0] = JSON.stringify(result);
     this.setState({ excelData: data });
   }
 
@@ -51,9 +52,9 @@ export default class InstructorFileChooser extends React.Component {
           reader.onload = this.excelToJson.bind(this, reader);
           reader.readAsBinaryString(input.files[0]);
           break;
-          case "csv":
-            reader.onload = this.csvToJson.bind(this, reader);
-            reader.readAsBinaryString(input.files[0]);
+        case "csv":
+          reader.onload = this.csvToJson.bind(this, reader);
+          reader.readAsBinaryString(input.files[0]);
         default:
           break;
       }
