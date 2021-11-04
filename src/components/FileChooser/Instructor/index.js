312954c648c32c96db6f691eb3c11b380d40361d
julianmendoza <julianjohnmendoza@gmail.com>
2021-11-03 22:05:29 -0400

    added csv and xlsx parser

--- src/components/FileChooser/Instructor/index.js
+++ src/components/FileChooser/Instructor/index.js
@@ -1,4 +1,5 @@
 import * as XLSX from "xlsx";
+import * as csvToJson from "convert-csv-to-json";
 import React from "react";
 export default class InstructorFileChooser extends React.Component {
   constructor(props) {
@@ -6,32 +7,56 @@ export default class InstructorFileChooser extends React.Component {
     this.state = { excelData: {} };
   }
   getState() {
-    let tmp=document.getElementById("contents");
-    tmp.innerHTML=this.state.excelData["Sheet1"];
+    let tmp = document.getElementById("contents");
+    tmp.innerHTML = this.state.excelData[0];
   }
   excelToJson(reader) {
     var fileData = reader.result;
     var wb = XLSX.read(fileData, { type: "binary" });
     var data = {};
-    wb.SheetNames.forEach(function (sheetName) {
-      var rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
+    for(let i=0;i<wb.SheetNames.length;i++){
+      var rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets["Sheet1"]);
       var rowString = JSON.stringify(rowObj);
-      data[sheetName] = rowString;
-    });
+      data[i] = rowString;
+    }
     this.setState({ excelData: data });
-    console.log(data["Sheet1"])
+  }
+  csvToJson(reader){
+    var fileData = reader.result;
+    var lines=fileData.split("\n")
+    var result = [];
+    var headers=lines[0].split(",");
+    var data = {};
+    for(var i=1;i<lines.length;i++){
+  
+        var obj = {};
+        var currentline=lines[i].split(",");
+  
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    data[0]=JSON.stringify(result)
+    this.setState({ excelData: data });
   }
 
   loadFileXLSX(event) {
     var input = event.target;
     var reader = new FileReader();
-    switch(input.files[0].name.split(".")[1]){
-      case "xlsx":
-        reader.onload = this.excelToJson.bind(this, reader);
-        reader.readAsBinaryString(input.files[0]);
-        break;
-    default:
-      break;
+    if (input.files[0] !== undefined) {
+      switch (input.files[0].name.split(".")[1]) {
+        case "xlsx":
+        case "xls":
+          reader.onload = this.excelToJson.bind(this, reader);
+          reader.readAsBinaryString(input.files[0]);
+          break;
+          case "csv":
+            reader.onload = this.csvToJson.bind(this, reader);
+            reader.readAsBinaryString(input.files[0]);
+        default:
+          break;
+      }
     }
   }
   render() {
@@ -39,7 +64,7 @@ export default class InstructorFileChooser extends React.Component {
       <div>
         <input type="file" onChange={this.loadFileXLSX.bind(this)} />
         <br />
-        <button onClick={()=>this.getState()}>Show Json</button>
+        <button onClick={() => this.getState()}>Show Json</button>
         <div id="contents"></div>
       </div>
     );
