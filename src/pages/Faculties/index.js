a5d37c3c1ad71dcedf0ca15915a6899d1d2f4574
John <johnwarde@cmail.carleton.ca>
2021-11-30 11:21:55 -0500

    test

---
+++ src/pages/Faculties/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../../components/NavBar'
+import DepartmentFileChooser from '../../components/FileChooser/Department'
+const Faculties = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            This is the department section
+            <DepartmentFileChooser></DepartmentFileChooser>
+       </div>
+    )
+}
+
+export default Faculties
