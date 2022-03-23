61ef42fb1ed5d9ba4e87b1367674382d425c95e2
julianmendoza <julianjohnmendoza@gmail.com>
2022-03-23 15:43:46 -0400

    for john

--- src/pages/Administrator/index.js
+++ src/pages/Administrator/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import NavBar from '../../components/NavBar'
 import AdminFileChooser from '../../components/FileChooser/Admin'
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import { CenterWrapper } from "../AllElements";
+import ReportGeneration from "../../components/ReportGeneration/reportGeneration";
 
 const Administrator = () => {
     return (
-        <div style={{backgroundColor:"white;"}}>
+        <div style={{backgroundColor:"white"}}>
             <NavBar></NavBar>
-            <AdminFileChooser></AdminFileChooser>
+            <Tabs defaultActiveKey="first">
+        <Tab eventKey="first" style={{backgroundColor:"#0d6efd"}} title="Admin Submission Requirements">
+          <AdminFileChooser></AdminFileChooser>
+        </Tab>
+        <Tab eventKey="second" title="Report Generation" style={{backgroundColor:"#0d6efd"}}>
+          <CenterWrapper >
+            <ReportGeneration></ReportGeneration>
+          </CenterWrapper>
+        </Tab>
+      </Tabs>
         </div>
     )
 }
